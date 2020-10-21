8a0dd0892d07578b6a7b795ec08b18e2ee0a7445
jinrui_ixnxn <wjr@ixnxn.com>
2020-10-21 18:44:03 +0800

    优化中间矩形透明绘制

--- LBXScan/UI/LBXScanView.m
+++ LBXScan/UI/LBXScanView.m
@@ -301,25 +301,19 @@ NS_ASSUME_NONNULL_END
                                  blue_notRecoginitonArea, alpa_notRecoginitonArea);
         
         //填充矩形
+        CGContextAddRect(context, self.bounds);
+        CGContextFillPath(context);
         
-        //扫码区域上面填充
-        CGRect rect = CGRectMake(0, 0, self.frame.size.width, YMinRetangle);
-        CGContextFillRect(context, rect);
+        //设置混合模式
+        CGContextSetBlendMode(context, kCGBlendModeClear);
+
+        // 设置中间区域透明
+        CGContextAddRect(context, CGRectMake(XRetangleLeft, YMinRetangle, sizeRetangle.width, sizeRetangle.height));
+        CGContextFillPath(context);
         
+        // 重置混合模式
+        CGContextSetBlendMode(context, kCGBlendModeNormal);
         
-        //扫码区域左边填充
-        rect = CGRectMake(0, YMinRetangle, XRetangleLeft,sizeRetangle.height);
-        CGContextFillRect(context, rect);
-        
-        //扫码区域右边填充
-        rect = CGRectMake(XRetangleRight, YMinRetangle, XRetangleLeft,sizeRetangle.height);
-        CGContextFillRect(context, rect);
-        
-        //扫码区域下面填充
-        rect = CGRectMake(0, YMaxRetangle, self.frame.size.width,self.frame.size.height - YMaxRetangle);
-        CGContextFillRect(context, rect);
-        //执行绘画
-        CGContextStrokePath(context);
     }
     
     if (_viewStyle.isNeedShowRetangle)
@@ -337,23 +331,23 @@ NS_ASSUME_NONNULL_END
        
     }
      _scanRetangleRect = CGRectMake(XRetangleLeft, YMinRetangle, sizeRetangle.width, sizeRetangle.height);
-    
-    
+
+
   //画矩形框4格外围相框角
-    
+
     //相框角的宽度和高度
     int wAngle = _viewStyle.photoframeAngleW;
     int hAngle = _viewStyle.photoframeAngleH;
-    
+
     //4个角的 线的宽度
     CGFloat linewidthAngle = _viewStyle.photoframeLineW;// 经验参数：6和4
-    
+
     //画扫码矩形以及周边半透明黑色坐标参数
     CGFloat diffAngle = 0.0f;
     //diffAngle = linewidthAngle / 2; //框外面4个角，与框有缝隙
     //diffAngle = linewidthAngle/2;  //框4个角 在线上加4个角效果
     //diffAngle = 0;//与矩形框重合
-    
+
     switch (_viewStyle.photoframeAngleStyle)
     {
         case LBXScanViewPhotoframeAngleStyle_Outer:
@@ -367,67 +361,67 @@ NS_ASSUME_NONNULL_END
         }
             break;
         case LBXScanViewPhotoframeAngleStyle_Inner:
-        {           
+        {
             diffAngle = -_viewStyle.photoframeLineW/2;
-            
+
         }
             break;
-            
+
         default:
         {
             diffAngle = linewidthAngle/3;
         }
             break;
     }
-    
+
     CGContextSetStrokeColorWithColor(context, _viewStyle.colorAngle.CGColor);
     CGContextSetRGBFillColor(context, 1.0, 1.0, 1.0, 1.0);
-    
+
     // Draw them with a 2.0 stroke width so they are a bit more visible.
     CGContextSetLineWidth(context, linewidthAngle);
-    
-    
+
+
     //
     CGFloat leftX = XRetangleLeft - diffAngle;
     CGFloat topY = YMinRetangle - diffAngle;
     CGFloat rightX = XRetangleRight + diffAngle;
     CGFloat bottomY = YMaxRetangle + diffAngle;
-    
+
     //左上角水平线
     CGContextMoveToPoint(context, leftX-linewidthAngle/2, topY);
     CGContextAddLineToPoint(context, leftX + wAngle, topY);
-    
+
     //左上角垂直线
     CGContextMoveToPoint(context, leftX, topY-linewidthAngle/2);
     CGContextAddLineToPoint(context, leftX, topY+hAngle);
-    
-    
+
+
     //左下角水平线
     CGContextMoveToPoint(context, leftX-linewidthAngle/2, bottomY);
     CGContextAddLineToPoint(context, leftX + wAngle, bottomY);
-    
+
     //左下角垂直线
     CGContextMoveToPoint(context, leftX, bottomY+linewidthAngle/2);
     CGContextAddLineToPoint(context, leftX, bottomY - hAngle);
-    
-    
+
+
     //右上角水平线
     CGContextMoveToPoint(context, rightX+linewidthAngle/2, topY);
     CGContextAddLineToPoint(context, rightX - wAngle, topY);
-    
+
     //右上角垂直线
     CGContextMoveToPoint(context, rightX, topY-linewidthAngle/2);
     CGContextAddLineToPoint(context, rightX, topY + hAngle);
-    
-    
+
+
     //右下角水平线
     CGContextMoveToPoint(context, rightX+linewidthAngle/2, bottomY);
     CGContextAddLineToPoint(context, rightX - wAngle, bottomY);
-    
+
     //右下角垂直线
     CGContextMoveToPoint(context, rightX, bottomY+linewidthAngle/2);
     CGContextAddLineToPoint(context, rightX, bottomY - hAngle);
-    
+
     CGContextStrokePath(context);
 }
 
